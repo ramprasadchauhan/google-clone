3fe30a41f4c39948f24bcdf2409f8d00614f9909
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-02-15 23:03:48 +0530

    search functionality

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const WebSearchPage = () => {
-  return <div>WebSearchPage</div>;
+const WebSearchPage = async ({ searchParams }) => {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_API_KEY}&cx=${process.env.NEXT_CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await response.json();
+  const results = data.items;
+  console.log(data);
+  return (
+    <div>
+      {results &&
+        results.map((result, i) => {
+          return <h1 key={i}>{result.title}</h1>;
+        })}
+      hii
+    </div>
+  );
 };
 
 export default WebSearchPage;
