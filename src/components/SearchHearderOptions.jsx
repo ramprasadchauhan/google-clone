c162869c691426164cfc2ad2f98fbdabb8218527
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-02-15 22:16:20 +0530

    complete search header section

--- src/components/SearchHearderOptions.jsx
+++ src/components/SearchHearderOptions.jsx
@@ -14,7 +14,7 @@ export default function SearchHeaderOptions() {
     );
   };
   return (
-    <div className="flex space-x-2 select-none border-b w-full justify-center lg:justify-start lg:pl-52 text-gray-700 text-sm">
+    <div className="flex space-x-2 mt-4 select-none border-b w-full justify-center lg:justify-start lg:pl-52 text-gray-700 text-sm">
       <div
         onClick={() => selectTab("All")}
         className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${
